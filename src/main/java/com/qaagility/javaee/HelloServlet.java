6dd4f72d71c4dfac14fbaf3f88b48dc05fd4957f
Test-Dev-ops <bipulranjankumar@gmail.com>
2018-03-18 11:12:55 +0530

    Update HelloServlet.java

--- src/main/java/com/qaagility/javaee/HelloServlet.java
+++ src/main/java/com/qaagility/javaee/HelloServlet.java
@@ -39,7 +39,7 @@ public class HelloServlet extends HttpServlet {
 	     out.println("<head>");
 	     out.println("<title>Hello World</title>");
 	     out.println("</head>");
-	     out.println("<body bgcolor='grey'>");
+	     out.println("<body bgcolor='red'>");
 		 out.println("<h1>Hello " + yourName + "</h1>");
 		 out.println("</body>");
 	     out.println("</html>");
